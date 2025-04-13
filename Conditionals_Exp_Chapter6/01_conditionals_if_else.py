e1a49c3f792ab7237ee997cf95dc84b56c599854
rvprakash9596 <raviprakash9596@gmail.com>
2025-04-13 19:53:03 +0530

    IF ELSE

---
+++ Conditionals_Exp_Chapter6/01_conditionals_if_else.py
@@ -0,0 +1,17 @@
+# Conditional expressions are used to make decisions in code. They evaluate a condition and execute a block of code depending on whether the condition is True or False.
+
+# Sn.      Type	                                Keyword(s)Used
+# 1.       Simple if Statement	                if
+# 2.       if...else Statement	                if, else
+# 3.       if...elif...else Statement	        if, elif, else
+# 4.       Ternary Conditional Expression	    x if condition else y
+
+age=int(input("Enter your age:"))
+if(age>=18):
+    print("You can vote")
+    print("Happy for you")
+else:
+    print("You can't vote")
+
+
+print("End of code")
