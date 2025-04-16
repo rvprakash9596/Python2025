0baa13def2f2831506ba6a9131440fbb8799bfce
rvprakash9596 <raviprakash9596@gmail.com>
2025-04-16 20:13:14 +0530

    Snake Water Gun Game

--- Project1/main.py
+++ Project1/main.py
@@ -1,4 +1,6 @@
-# PROJECT 1: SNAKE, WATER, GUN GAME :: We all have played snake, water gun game in our childhood. If you haven’t, google the rules of this game and write a python program capable of playing this game with the
+#                               PROJECT 1: SNAKE, WATER, GUN GAME
+#                           -------------------------------------------
+# We all have played snake, water gun game in our childhood. If you haven’t, google the rules of this game and write a python program capable of playing this game with the
 
 '''
 1 for snake
