45bc5c5a7d1e35b50b1a6ea9af7e979a77d0204c
rvprakash9596 <raviprakash9596@gmail.com>
2025-04-16 20:36:12 +0530

    THE PERFECT GUESS

---
+++ Project2/main.py
@@ -0,0 +1,16 @@
+import random
+
+n = random.randint(1, 100)
+a = -1
+guesses = 0
+
+while(a != n):
+    a = int(input("Guess The Number: "))
+    if(a > n):
+        print("Lower Number Please")
+        guesses += 1
+    elif(a<n):
+        print("Higher Number Please")
+        guesses += 1
+
+print(f"Congratulations! You have guessed the number {n} correctly in {guesses} attempts.")
